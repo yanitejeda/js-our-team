7b4d7b45ff9eef2771ca1f79859217172c9a742b
yanitejeda <tejedasp@gmail.com>
2021-10-21 19:23:58 +0200

    terzo salvataggio parziale

--- script.js
+++ script.js
@@ -6,44 +6,93 @@
   //- Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team.
    
 
+  const teamContainer = document.querySelector(".team-container"); //prendo l'elemento nel mio dom dove devo inserire le card
+    console.log(teamContainer);
 
+    const addMemberButton = document.getElementById("addMemberButton") //prenso il bottone
+    
+    
   //per prima cosa creare un OBJECT con i vari membri del team
-
-  const membriTeam = [
-      {
-          'nome':'Wayne Barnett',
-          'text':'Founder & CEO',
-          'image':'wayne-barnett-founder-ceo.jpg',
-      },
-      {
-        'nome':'Angela Caroll',
-        'text':'Chief Editor',
-        'image':'angela-caroll-chief-editor.jpg',
+let gruppoPersonaggi = {singleElement : [
+    {
+        nome:'Wayne Barnett',
+        text:'Founder & CEO',
+        image:'wayne-barnett-founder-ceo.jpg',
     },
     {
-        'nome':'Walter Gordon',
-        'text':'Office Manager',
-        'image':'walter-gordon-office-manager.jpg',
-    },
-    {
-        'nome':'Angela Lopez',
-        'text':'Social Media Manager',
-        'image':'angela-lopez-social-media-manager.jpg',
-    },
-    {
-        'nome':'Scott Estrada',
-        'text':'Developer',
-        'image':'scott-estrada-developer.jpg',
-    },
-    {
-        'nome':'Barbara Ramos',
-        'text':'Graphic Designer',
-        'image':'barbara-ramos-graphic-designer.jpg',
-    },
-  ];
-
-  for (let key in membriTeam) {
-      console.log(membriTeam[key]);
+      nome:'Angela Caroll',
+      text:'Chief Editor',
+      image:'angela-caroll-chief-editor.jpg',
+  },
+  {
+      nome:'Walter Gordon',
+      text:'Office Manager',
+      image:'walter-gordon-office-manager.jpg',
+  },
+  {
+      nome:'Angela Lopez',
+      text:'Social Media Manager',
+      image:'angela-lopez-social-media-manager.jpg',
+  },
+  {
+      nome:'Scott Estrada',
+      text:'Developer',
+      image:'scott-estrada-developer.jpg',
+  },
+  {
+      nome:'Barbara Ramos',
+      text:'Graphic Designer',
+      image:'barbara-ramos-graphic-designer.jpg',
   }
+]
+
+};
+
+
 
  
+  //tramite un ciclo for seleziono i singoli elementi nel mio object
+let personaggi = gruppoPersonaggi['singleElement']; 
+
+
+for (let i = 0, len = personaggi.length; i<len; i++) {
+    
+    console.log(personaggi[i]);
+    console.log(personaggi[i].nome);
+    console.log(personaggi[i].text);
+    console.log(personaggi[i].image);
+
+    teamContainer.innerHTML += `
+
+    <div class="team-card">
+        <div class="card-image">
+          <img
+            src="${personaggi[i].image}"
+            alt="${personaggi[i].nome}"
+          />
+        </div>
+        <div class="card-text">
+          <h3>${personaggi[i].nome}</h3>
+          <p>${personaggi[i].text}</p>
+        </div>
+      </div>
+    
+
+
+    `;
+
+}
+
+//al clic del bottone l'utente può aggiungere un nuovo membro
+//bisogna creare un bottone che al suo click compie un'azione
+addMemberButton.addEventListener('click', function(){
+    personaggi.push({
+        nome:'pino',
+        text:'',
+        image:'',
+    })
+    console.log(personaggi);
+    
+   
+
+})
